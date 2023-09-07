884af189551dadcda43dbeb81ebab0a41a0c325c
Jhony Alpala <jhoniher95@gmail.com>
2023-09-06 20:01:20 -0500

    Agregada una impresión

--- Problema3.java
+++ Problema3.java
@@ -3,5 +3,6 @@ import java.util.Scanner;
 public class Problema3 {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
+        System.out.println("Este comentario es una prueba");
     }
 }
