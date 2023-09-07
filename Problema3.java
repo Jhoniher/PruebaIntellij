d0e1c9811e41492a62f39a84e8c11d126a0381b0
Jhony Alpala <jhoniher95@gmail.com>
2023-09-06 21:25:50 -0500

    cambios guardados

--- Problema3.java
+++ Problema3.java
@@ -4,5 +4,9 @@ public class Problema3 {
     public static void main(String[] args) {
         Scanner entrada=new Scanner(System.in);
         System.out.println("Este comentario es una prueba");
+        int i=10;
+        for(i; 100;++){
+
+        }
+        }
     }
-}
